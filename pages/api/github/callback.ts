afe74728277e780cafa69682f3879f4b2cf33daf
lucas <lucas.vergara@usm.cl>
2020-11-06 16:04:16 -0300

    add github callback

---
+++ pages/api/github/callback.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+interface GithubBody {
+    client_id: string,
+    client_secret: string,
+    code: string,
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const code = req.query.code as string
+    const body: GithubBody = {
+        client_id: '4d39a3c2879f21dc07d5',
+        client_secret: 'bc308ddc14a44a5048eb27e12006c639df489f51',
+        code
+    }
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+    })
+    const json = await response.json()
+    res.redirect('http://localhost:3000/auth?code='+json.access_token)
+}
