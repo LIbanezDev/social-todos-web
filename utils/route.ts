1c365f01c83d657a8909b6b054f49ae05942ab4f
lucas <lucas.vergara@usm.cl>
2020-11-06 16:00:37 -0300

    route utils

---
+++ utils/route.ts
@@ -0,0 +1,25 @@
+export const toParams = (query: string): object => {
+    const q = query.replace(/^\??\//, '');
+
+    return q.split('&').reduce((values, param) => {
+        const [key, value] = param.split('=');
+
+        values[key] = value;
+
+        return values;
+    }, {});
+}
+
+export const toQuery = (params: object, delimiter = '&'): string => {
+    const keys = Object.keys(params);
+
+    return keys.reduce((str, key, index) => {
+        let query = `${str}${key}=${params[key]}`;
+
+        if (index < (keys.length - 1)) {
+            query += delimiter;
+        }
+
+        return query;
+    }, '');
+}
