9521186345aa4bbff232df228dee1a2d28d0564d
lucas <lucas.vergara@usm.cl>
2020-11-17 17:30:50 -0300

    add Image component to gifs

--- components/Gifs.tsx
+++ components/Gifs.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { GetTrendingGifsQuery } from '../__generated__/GraphQLTypes';
 import { Typography } from '@material-ui/core';
+import Image from 'next/image';
 
 const Gifs = ({ queryResult }: { queryResult: GetTrendingGifsQuery }) => {
 	return (
 		<Typography>
 			{queryResult.trendingGifs.map(gif => (
-				<img
-					height='400'
-					width='400'
+				<Image
+					height='300'
+					width='300'
 					src={gif.images.fixed_height.url}
 					alt={gif.title}
 					key={gif.id}
