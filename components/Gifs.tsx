5cf08ce395cfa022146825ffdfbfb7e18924778a
lucas <lucas.vergara@usm.cl>
2020-11-17 04:48:39 -0300

    update next js 10

--- components/Gifs.tsx
+++ components/Gifs.tsx
@@ -5,15 +5,15 @@ import { Typography } from '@material-ui/core';
 const Gifs = ({ queryResult }: { queryResult: GetTrendingGifsQuery }) => {
 	return (
 		<Typography>
-			{queryResult.trendingGifs.map(gif => {
-				return (
-					<img
-						src={gif.images.fixed_height.url}
-						alt={gif.title}
-						key={gif.id}
-					/>
-				);
-			})}
+			{queryResult.trendingGifs.map(gif => (
+				<img
+					height='400'
+					width='400'
+					src={gif.images.fixed_height.url}
+					alt={gif.title}
+					key={gif.id}
+				/>
+			))}
 			{JSON.stringify(queryResult, null, 4)}
 		</Typography>
 	);
